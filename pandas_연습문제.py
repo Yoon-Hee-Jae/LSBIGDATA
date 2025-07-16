ac22ec3b8a51263261e22a2d32f909cf0c73c0fa
Yoon-Hee-Jae <gmlwo2969@naver.com>
2025-07-16 11:25:56 +0900

    7월 16일 수업내용

--- pandas_연습문제.py
+++ pandas_연습문제.py
@@ -29,7 +29,25 @@ print(max_ass,'/n',min_ass)
 df['average'] = df[["midterm","final","assignment"]].mean(axis=1)
 answer_10 = df.groupby('gender')[['assignment','average','final','midterm']].mean()
 type(answer_10)
-df
+answer_10
+# 실제 문제 풀이 답안
+# reset_index 사용하면 그룹별 첫 행만이 아닌 모든 행에 그룹 이름이 출력됨
+pd.melt(df,
+        id_vars=['gender'],
+        var_name='variable',
+        value_vars=["midterm",'final','assignment','average'],
+        value_name='score').groupby(['gender','variable']).mean().reset_index()
+
+# 강사님 풀이
+df_1 = df.groupby('gender')[["midterm",'final','assignment','average']].mean()
+df_1 # 이때 gender는 1번째 열이 아닌 index로 설정되어 있기 때문에 조정이 필요하다
+# group by 인수로 as_index=False로 설정 // index가 0,1로 바뀐 것을 확인 가능
+result = df.groupby('gender',as_index=False)[["midterm",'final','assignment','average']].mean()
+result
+result = pd.melt(result,id_vars='gender',var_name='variable',value_name='score')
+result = result.sort_values(['gender','variable'])
+result.reset_index()
+
 # 11번 중간 기말 과제의 평균을 구하고 average열을 생성하세요
 # 최대 평균 성적을 가진 학생의 이름과 평균 성적을 출력하세요
 df
@@ -38,4 +56,3 @@ max_mean_idx
 
 df.loc[max_mean_idx,['name','average']] # 답안
 
-asda
