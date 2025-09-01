b3c9456ef0964d77838eac30236db30eb446cf83
Yoon-Hee-Jae <gmlwo2969@naver.com>
2025-09-01 20:22:22 +0900

    ,

--- [머신러닝]데이터전처리개요.py
+++ [머신러닝]데이터전처리개요.py
@@ -2,3 +2,162 @@ import pandas as pd
 import numpy as np
 
 dat = pd.read_csv('https://raw.githubusercontent.com/YoungjinBD/data/main/dat.csv')
+dat.info()
+dat.head()
+y = dat.grade
+X = dat.drop(['grade'], axis = 1)
+X['school'].value_counts()
+# 데이터 분할
+from sklearn.model_selection import train_test_split
+train_X, test_X, train_y, test_y = train_test_split(
+                   X,  
+                   y, 
+                   test_size = 0.2,
+                   random_state = 0,
+                   shuffle = True,
+                   stratify = None
+                   )
+
+print('trainX shape : ',  train_X.shape)
+print('trainy shape : ',  train_y.shape)
+print('testX shape : ',  test_X.shape)
+print('testy shape : ',  test_y.shape)
+
+# train test y 히스토그렘
+import matplotlib.pyplot as plt
+fig, axs = plt.subplots(nrows=1, ncols=2)
+train_y.hist(ax=axs[0], color='blue', alpha=0.7)
+axs[0].set_title('histogram of train y')
+test_y.hist(ax=axs[1], color='red', alpha=0.7)
+axs[1].set_title('histogram of test y')
+plt.tight_layout()
+plt.show()
+
+# train test y의 분포가 같은지 확인하는 검정 = two sample ks 검정
+from scipy.stats import ks_2samp
+
+# train_y와 test_y 분포 비교
+stat, p_value = ks_2samp(train_y, test_y)
+
+print(f"KS statistic: {stat:.4f}")
+print(f"p-value: {p_value:.4f}")
+
+if p_value > 0.05:
+    print("Train과 Test의 분포가 비슷합니다. ✅")
+else:
+    print("Train과 Test의 분포가 다릅니다. ⚠️")
+
+# 층화 추출을 통해 train/test 분할
+train_X, test_X, train_y, test_y = train_test_split(
+                    X, 
+                    y, 
+                    test_size = 0.2,                                                        stratify = X['school'], 
+                    random_state = 0)
+# 분포 비교
+import matplotlib.pyplot as plt
+fig, axs = plt.subplots(nrows=1, ncols=2)
+train_y.hist(ax=axs[0], color='blue', alpha=0.7)
+axs[0].set_title('histogram of train y')
+test_y.hist(ax=axs[1], color='red', alpha=0.7)
+axs[1].set_title('histogram of test y')
+plt.tight_layout(); 
+plt.show();
+
+# school 분포 비교
+# train/test 'school' 값의 빈도 계산
+train_counts = train_X['school'].value_counts().sort_index()
+test_counts = test_X['school'].value_counts().sort_index()
+
+# figure 생성
+fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10,4))
+
+# train bar plot
+axs[0].bar(train_counts.index, train_counts.values, color='blue', alpha=0.7)
+axs[0].set_title('Train school distribution')
+axs[0].set_xlabel('School')
+axs[0].set_ylabel('Count')
+
+# test bar plot
+axs[1].bar(test_counts.index, test_counts.values, color='red', alpha=0.7)
+axs[1].set_title('Test school distribution')
+axs[1].set_xlabel('School')
+axs[1].set_ylabel('Count')
+
+plt.tight_layout()
+plt.show()
+
+# 결측치 처리 - 평균대치법
+dat = pd.read_csv('https://raw.githubusercontent.com/YoungjinBD/data/main/dat.csv')
+print(dat.isna().sum(axis = 0))
+
+y = dat.grade
+X = dat.drop(['grade'], axis = 1)
+
+from sklearn.model_selection import train_test_split
+train_X, test_X, train_y, test_y = train_test_split(
+                   X,  
+                   y, 
+                   test_size = 0.2,                     
+                   random_state = 0, 
+                   shuffle = True, 
+                   stratify = None 
+                   )
+
+from sklearn.impute import SimpleImputer
+train_X1 = train_X.copy()
+test_X1 = test_X.copy()
+imputer_mean = SimpleImputer(strategy = 'mean')
+train_X1['goout'] = imputer_mean.fit_transform(train_X1[['goout']])
+test_X1['goout'] = imputer_mean.transform(test_X1[['goout']])
+print('학습 데이터 goout 변수 결측치 확인 :', train_X1['goout'].isna().sum())
+
+# 결츠기 처리 - knn
+from sklearn.impute import KNNImputer
+train_X5 = train_X.copy()
+test_X5 = test_X.copy()
+train_X5_num = train_X5.select_dtypes('number')
+test_X5_num = test_X5.select_dtypes('number')
+train_X5_cat = train_X5.select_dtypes('object')
+test_X5_cat = test_X5.select_dtypes('object')
+
+knnimputer = KNNImputer(n_neighbors = 5)
+train_X5_num_imputed = knnimputer.fit_transform(train_X5_num)
+test_X5_num_imputed = knnimputer.transform(test_X5_num)
+                       
+train_X5_num_imputed = pd.DataFrame(train_X5_num_imputed, 
+                                    columns=train_X5_num.columns, 
+                                    index = train_X5.index)
+test_X5_num_imputed = pd.DataFrame(test_X5_num_imputed, 
+                                   columns=test_X5_num.columns, 
+                                   index = test_X5.index)
+train_X5 = pd.concat([train_X5_cat, train_X5_num_imputed], axis = 1)
+test_X5 = pd.concat([test_X5_cat, test_X5_num_imputed], axis = 1)
+print('학습 데이터 goout 변수 결측치 확인 :', train_X5['goout'].isna().sum())
+
+# 출력결과를 numpy가 아닌 pandas로 바로 출력되도록해줌 .set_output
+knnimputer2 = KNNImputer(n_neighbors = 5).set_output(transform = 'pandas')
+train_X5_num_imputed2 = knnimputer2.fit_transform(train_X5_num)
+test_X5_num_imputed2 = knnimputer2.transform(test_X5_num)
+# 판다스 데이터프레임 출력 
+print(train_X5_num_imputed2.head())
+
+# 변수 변환 방법
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import PowerTransformer
+import warnings
+np.warnings = warnings
+
+bike_data = pd.read_csv("https://raw.githubusercontent.com/YoungjinBD/data/main/bike_train.csv")
+
+import matplotlib.pyplot as plt
+bike_data['count'].hist();
+plt.show();
+
+box_tr = PowerTransformer(method = 'box-cox')
+bike_data['count_boxcox'] = box_tr.fit_transform(
+    bike_data[['count']])
+print('lambda : ', box_tr.lambdas_)
+
+bike_data['count_boxcox'].hist();
+plt.show();
--- lec24-houseprice.py
+++ lec24-houseprice.py
@@ -41,6 +41,8 @@ submit_data.to_csv('./data/house_prediction/second_submit.csv',index=False,
 train_data = pd.read_csv('./data/house_prediction/train.csv')
 test_data = pd.read_csv('./data/house_prediction/test.csv')
 submit_data = pd.read_csv('./data/house_prediction/sample_submission.csv')
+train_data.info()
+test_data.info()
 
 # 1. 의미 없는 변수 제거 ID 칼럼
 numeric_train = train_data.select_dtypes(include=['int64','float64'])
@@ -74,3 +76,217 @@ submit_data.to_csv('tree2.cs' \
 'v',index=False,
                  encoding='utf-8')
 
+###########################################################################
+
+# 사이킷런 활용해서 데이터 누수 없이 성능 올리기
+train_data = pd.read_csv('./data/house_prediction/train.csv')
+test_data = pd.read_csv('./data/house_prediction/test.csv')
+submit_data = pd.read_csv('./data/house_prediction/sample_submission.csv')
+train_data.info()
+test_data.info()
+
+# 결측치 제거
+train_data.dropna(subset=['MasVnrArea'],inplace=True)
+
+# 데이터 분리
+train_X = train_data.drop(['SalePrice'], axis = 1)
+train_y = train_data['SalePrice']
+
+# train 데이터 train/validation 으로 분할
+from sklearn.model_selection import train_test_split
+train_X_sub, valid_X, train_y_sub, valid_y = train_test_split(train_X, train_y, test_size = 0.3, random_state = 1)
+
+# 수치형 변수만 선택
+numeric_train = train_X_sub.select_dtypes(include=['int64','float64'])
+numeric_val = valid_X.select_dtypes(include=['int64','float64'])
+numeric_test = test_data.select_dtypes(include=['int64','float64'])
+
+# id 칼럼 제거
+numeric_train = numeric_train.drop('Id',axis=1)
+numeric_val = numeric_val.drop('Id',axis=1)
+numeric_test = numeric_test.drop('Id',axis=1)
+
+# 평균값 대체
+numeric_train.info()
+from sklearn.impute import SimpleImputer
+imputer_mean = SimpleImputer(strategy = 'most_frequent').set_output(transform='pandas')
+# LotFrontage
+numeric_train = imputer_mean.fit_transform(numeric_train)
+numeric_train.isna().sum()
+numeric_val = imputer_mean.transform(numeric_val)
+numeric_val.isna().sum()
+numeric_test = imputer_mean.transform(numeric_test)
+numeric_train.isna().sum()
+
+# 모델 학습
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+lr.fit(numeric_train, train_y_sub)
+
+# 모델 평가
+from sklearn.metrics import root_mean_squared_error
+pred_val = lr.predict(numeric_val)
+print('valid RMSE:' , root_mean_squared_error(valid_y, pred_val))
+
+# mean = 31181.317716614016
+# median = 31184.179231714752
+# most_frequent = 31103.01163287705
+# knn = 31190.37533084124
+
+# knn
+# 사이킷런 활용해서 데이터 누수 없이 성능 올리기
+train_data = pd.read_csv('./data/house_prediction/train.csv')
+test_data = pd.read_csv('./data/house_prediction/test.csv')
+submit_data = pd.read_csv('./data/house_prediction/sample_submission.csv')
+
+# 결측치 제거
+train_data.dropna(subset=['MasVnrArea'],inplace=True)
+
+# 데이터 분리
+train_X = train_data.drop(['SalePrice'], axis = 1)
+train_y = train_data['SalePrice']
+
+# train 데이터 train/validation 으로 분할
+from sklearn.model_selection import train_test_split
+train_X_sub, valid_X, train_y_sub, valid_y = train_test_split(train_X, train_y, test_size = 0.3, random_state = 1)
+
+# 수치형 변수만 선택
+numeric_train = train_X_sub.select_dtypes(include=['int64','float64'])
+numeric_val = valid_X.select_dtypes(include=['int64','float64'])
+numeric_test = test_data.select_dtypes(include=['int64','float64'])
+
+# id 칼럼 제거
+numeric_train = numeric_train.drop('Id',axis=1)
+numeric_val = numeric_val.drop('Id',axis=1)
+numeric_test = numeric_test.drop('Id',axis=1)
+
+# knn 대체
+from sklearn.impute import KNNImputer
+knnimputer = KNNImputer(n_neighbors = 5)
+
+train_X5_num_imputed = knnimputer.fit_transform(numeric_train)
+val_X5_num_imputed = knnimputer.transform(numeric_val)
+test_X5_num_imputed = knnimputer.transform(numeric_test)
+                       
+train_X5_num_imputed = pd.DataFrame(train_X5_num_imputed, 
+                                    columns=numeric_train.columns, 
+                                    index = numeric_train.index)
+val_X5_num_imputed = pd.DataFrame(val_X5_num_imputed, 
+                                   columns=numeric_val.columns, 
+                                   index = numeric_val.index)
+test_X5 = pd.DataFrame(test_X5_num_imputed, 
+                       columns=numeric_test.columns,
+                       index=numeric_test.index)
+
+train_X5 = train_X5_num_imputed
+val_X5 = val_X5_num_imputed
+test_X5
+print('학습 데이터 변수 결측치 확인 :', train_X5.isna().sum())
+
+# 모델 학습
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+lr.fit(train_X5, train_y_sub)
+
+# 모델 평가
+from sklearn.metrics import root_mean_squared_error
+pred_val = lr.predict(val_X5)
+print('valid RMSE:' , root_mean_squared_error(valid_y, pred_val))
+# 최종 test 데이터 예측
+pred_test = lr.predict(test_X5)
+submit_data['SalePrice'] = pred_test
+submit_data.to_csv('submission1631.csv', index=False)
+
+print("최종 예측 완료! submission.csv 생성됨")
+
+
+##############3
+# 최종
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LinearRegression
+
+# 데이터 불러오기
+train_data = pd.read_csv('./data/house_prediction/train.csv')
+test_data = pd.read_csv('./data/house_prediction/test.csv')
+submit_data = pd.read_csv('./data/house_prediction/sample_submission.csv')
+
+# 결측치 제거 (필요한 경우)
+train_data.dropna(subset=['MasVnrArea'], inplace=True)
+
+# X, y 분리
+train_X = train_data.drop(['SalePrice'], axis=1)
+train_y = train_data['SalePrice']
+
+# 수치형 변수만 선택
+numeric_train = train_X.select_dtypes(include=['int64','float64']).drop('Id', axis=1)
+numeric_test = test_data.select_dtypes(include=['int64','float64']).drop('Id', axis=1)
+
+# 최빈값 대체 (train 전체 데이터에 fit)
+imputer = SimpleImputer(strategy='most_frequent').set_output(transform='pandas')
+numeric_train = imputer.fit_transform(numeric_train)
+numeric_test = imputer.transform(numeric_test)
+
+# 모델 학습 (전체 train 데이터 사용)
+lr = LinearRegression()
+lr.fit(numeric_train, train_y)
+
+# 최종 test 데이터 예측
+pred_test = lr.predict(numeric_test)
+
+# 제출파일 생성
+submit_data['SalePrice'] = pred_test
+submit_data.to_csv('submission1630.csv', index=False)
+
+print("최종 예측 완료! submission.csv 생성됨")
+
+# 
+
+
+
+
+# 데이터 불러오기
+train_data = pd.read_csv('./data/house_prediction/train.csv')
+test_data = pd.read_csv('./data/house_prediction/test.csv')
+submit_data = pd.read_csv('./data/house_prediction/sample_submission.csv')
+
+# 결측치 제거 (필요한 경우)
+train_data.dropna(subset=['MasVnrArea'], inplace=True)
+
+# X, y 분리
+train_X = train_data.drop(['SalePrice'], axis=1)
+train_y = train_data['SalePrice']
+
+# 수치형 변수만 선택
+numeric_train = train_X.select_dtypes(include=['int64','float64']).drop('Id', axis=1)
+numeric_test = test_data.select_dtypes(include=['int64','float64']).drop('Id', axis=1)
+
+# 최빈값 대체 (train 전체 데이터에 fit)
+imputer = SimpleImputer(strategy='most_frequent').set_output(transform='pandas')
+numeric_train = imputer.fit_transform(numeric_train)
+numeric_test = imputer.transform(numeric_test)
+
+# 표준화
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+stdscaler = StandardScaler()
+mc_transformer = make_column_transformer(
+    (stdscaler, numeric_train.columns),
+    remainder='passthrough'
+    ).set_output(transform = 'pandas')
+    
+train_X_transformed = mc_transformer.fit_transform(numeric_train)
+test_X_transformed = mc_transformer.transform(numeric_test)
+
+# 모델 학습 (전체 train 데이터 사용)
+lr = LinearRegression()
+lr.fit(train_X_transformed, train_y)
+
+# 최종 test 데이터 예측
+pred_test = lr.predict(test_X_transformed)
+
+# 제출파일 생성
+submit_data['SalePrice'] = pred_test
+submit_data.to_csv('./data/house_prediction/submission1731.csv', index=False)
+
+print("최종 예측 완료! submission.csv 생성됨")
